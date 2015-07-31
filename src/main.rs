41456491d98fd9eb2025c27ec490df983bc2946e
Daniel Wehner <daniel@tag1consulting.com>
2015-07-31 12:33:21 +0200

    first basic version without any random data yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_length: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    // Write the Docopt usage string.
+    static USAGE: &'static str = "
+    Usage: rpg [--length=<length>]
+
+    Options:
+        --length=<length>     Length of the random string.
+    ";
+
+    // let argv = std::env::args();
+
+    let args: Args = Docopt::new(USAGE)
+                  .and_then(|d| d.decode())
+                  .unwrap_or_else(|e| e.exit());
+
+    println!("{}", args.flag_length);
 }
