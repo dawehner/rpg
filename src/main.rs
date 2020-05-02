259acbde8c6f9cb3630a83d00cfce997c8a3d0fc
Daniel Wehner <dawehner@googlemail.com>
2020-05-02 19:50:15 +0100

    Implement entropy calculation in verbose mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate rand;
+extern crate num_bigint;
+extern crate num_traits;
+extern crate num;
 
 use docopt::Docopt;
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use num_bigint::{BigInt};
+use num::pow::pow;
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
     flag_length: usize,
     flag_type: TargetMode,
+    flag_verbose: bool
 }
 
 #[derive(Debug)]
@@ -31,19 +37,26 @@ impl rustc_serialize::Decodable for TargetMode {
         })
     }
 }
-    
+
+fn calculate_entropy(length : usize, mode : TargetMode) -> BigInt {
+    match mode {
+        TargetMode::String => pow(BigInt::from(62), length),
+        TargetMode::Number => pow(BigInt::from(10), length),
+    }
+}
 
 fn main() {
 
     // Write the Docopt usage string.
     static USAGE: &'static str = "
     Usage:
-      rpg [--length=<length>] [--type=<type>]
+      rpg [--length=<length>] [--type=<type>] [--verbose]
       rpg (-h | --help)
 
     Options:
         -h --help             Show this screen.
         --length=<length>     Length of the random string [default: 20].
+        --verbose             Shows information about the password
         --type=<type>         Target type: s, string for string, n, number for number [default: s]
     ";
 
@@ -59,6 +72,12 @@ fn main() {
         // @fixme It would be better to just generate a bunch of 1 char numbers.
         TargetMode::Number => thread_rng().gen_range(0, ten.pow(args.flag_length as u32)).to_string(),
     };
+
+    if args.flag_verbose {
+        println!("Length: {}", args.flag_length);
+        println!("Entropy: {}", calculate_entropy(args.flag_length, args.flag_type));
+        println!("");
+    }
 
     println!("{}", random_string);
 }
